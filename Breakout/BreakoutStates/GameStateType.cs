b174963a40a62d9c87d6222a664d72c4475dcc10
student <kmn689@alumni.ku.dk>
2024-04-23 14:25:54 +0000

    States update

--- Breakout/BreakoutStates/GameStateType.cs
+++ Breakout/BreakoutStates/GameStateType.cs
@@ -10,4 +10,26 @@ using DIKUArcade.Graphics;
 using System.Collections.Generic;
 
 namespace Breakout.BreakoutStates;
-public enum 
+public enum GameStateType {
+    GameRunning,
+}
+
+public class StateTransformer {
+    public static GameStateType TransformStringToState(string state) {
+        switch (state) {
+            case "GAME_RUNNING":
+                return GameStateType.GameRunning;
+            default:
+                throw new ArgumentException("Invalid state type");
+        }
+    }
+
+    public static string TransformStateToString(GameStateType state) {
+        switch (state) {
+            case GameStateType.GameRunning:
+                return "GAME_RUNNING";
+            default:
+                throw new ArgumentException("Invalid state type");
+        }
+    }
+}
