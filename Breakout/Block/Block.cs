65ebefe79386ad393da3895f707e3c440bf1899d
student <kmn689@alumni.ku.dk>
2024-04-24 18:02:18 +0000

    block interface

---
+++ Breakout/Block/Block.cs
@@ -0,0 +1,38 @@
+using System;
+using System.IO;
+using DIKUArcade.Entities;
+using DIKUArcade.Graphics;
+using DIKUArcade.Math;
+using DIKUArcade;
+using DIKUArcade.Events;
+using DIKUArcade.Input;
+using System.Collections.Generic;
+using DIKUArcade.GUI;
+using DIKUArcade.Physics;
+
+namespace Breakout.IBlock;
+public class Block : Entity, IBlock {
+    private Vec2F startPos; 
+    private int health = 30;
+    private IBaseImage blocksImage;
+
+    public int Health {
+        get { return health; }
+    }
+
+    public Block(DynamicShape shape, IBaseImage blocksImage) : base(shape, blocksImage) {
+        this.blocksImage = blocksImage;
+        this.startPos = shape.Position;
+    }
+
+    public void Damage() {
+        health -= 10;
+        if (health <= 0) {
+            Destroy();
+        }
+    }
+
+    private void Destroy() {
+        DeleteEntity();
+    }
+}
