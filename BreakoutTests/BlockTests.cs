0bf83916c2ac0f33d39410f06c84c0db497870de
Sebastian <sebastianmcconnell01@gmail.com>
2024-05-23 17:55:40 +0000

    new block tests

--- BreakoutTests/BlockTests.cs
+++ BreakoutTests/BlockTests.cs
@@ -1,6 +1,7 @@
 using NUnit.Framework;
 using Breakout;
 using Breakout.IBlock;
+using Breakout.PowerUps;
 using System;
 using System.IO;
 using DIKUArcade.Entities;
@@ -22,6 +23,9 @@ public class BlockTest {
     private Hardened hardened;
     private Unbreakable unbreakable;
     private EntityContainer<Block> blocks;
+    string testFilePath = Path.Combine(
+            "..", "..", "..", "..", "Assets", "Levels", "level1.txt"
+        );
     [OneTimeSetUp]
     public void Init() {
         DIKUArcade.GUI.Window.CreateOpenGLContext();
@@ -43,6 +47,7 @@ public class BlockTest {
         blocks.AddEntity(block);
         blocks.AddEntity(hardened);
         blocks.AddEntity(unbreakable);
+        GameRunning gameRunning = new GameRunning(testFilePath);
     }
     [Test]
     public void TestBlockHealth() {
@@ -116,6 +121,126 @@ public class BlockTest {
         IBaseImage image = img;
         IBaseImage image1 = hardened1.GetImage();
         Assert.AreEqual(image, image1);
+    }
+
+    [Test]
+    public void powerUpBlockSpawn() {
+        var shape = new DynamicShape(new Vec2F(0.1f, 0.1f),
+            new Vec2F(0.2f, 0.2f));
+        var img = new Image(Path.Combine("..", "Assets", "Images", "red-block.png"));
+        var img1 = new Image(Path.Combine("..", "Assets", "Images", "red-block-damaged.png"));
+        PowerUpBlock powerUp = new PowerUpBlock(shape, img, img1, BlockType.PowerUp);
+        powerUp.SpawnPowerUp();
+        Assert.AreEqual(GameRunning.Effects.CountEntities(), 1);
+    }
+
+    [Test]
+    public void DoubleSizeToObjectTest() {
+        var shape = new DynamicShape(new Vec2F(0.1f, 0.1f),
+            new Vec2F(0.2f, 0.2f));
+        var img = new Image(Path.Combine("..", "Assets", "Images", "red-block.png"));
+        var img1 = new Image(Path.Combine("..", "Assets", "Images", "red-block-damaged.png"));
+        PowerUpBlock powerUp = new PowerUpBlock(shape, img, img1, BlockType.PowerUp);
+        Effect doubleSize = powerUp.PowerUpTypeToObject(EffectType.DoubleSize, img);
+        Effect expectedDS = new DoubleSize(
+            new DynamicShape(powerUp.Shape.Position, powerUp.Shape.Extent), img
+        );
+        Assert.AreEqual(doubleSize.GetType(), expectedDS.GetType());
+    }
+
+    [Test]
+    public void ExtraLifeToObjectTest() {
+        var shape = new DynamicShape(new Vec2F(0.1f, 0.1f),
+            new Vec2F(0.2f, 0.2f));
+        var img = new Image(Path.Combine("..", "Assets", "Images", "red-block.png"));
+        var img1 = new Image(Path.Combine("..", "Assets", "Images", "red-block-damaged.png"));
+        PowerUpBlock powerUp = new PowerUpBlock(shape, img, img1, BlockType.PowerUp);
+        Effect extraLife = powerUp.PowerUpTypeToObject(EffectType.ExtraLife, img);
+        Effect expectedEL = new ExtraLife(
+            new DynamicShape(powerUp.Shape.Position, powerUp.Shape.Extent), img
+        );
+        Assert.AreEqual(extraLife.GetType(), expectedEL.GetType());
+    }
+
+    [Test]
+    public void PlayerSpeedToObjectTest() {
+        var shape = new DynamicShape(new Vec2F(0.1f, 0.1f),
+            new Vec2F(0.2f, 0.2f));
+        var img = new Image(Path.Combine("..", "Assets", "Images", "red-block.png"));
+        var img1 = new Image(Path.Combine("..", "Assets", "Images", "red-block-damaged.png"));
+        PowerUpBlock powerUp = new PowerUpBlock(shape, img, img1, BlockType.PowerUp);
+        Effect playerSpeed = powerUp.PowerUpTypeToObject(EffectType.PlayerSpeed, img);
+        Effect expectedPS = new PlayerSpeed(
+            new DynamicShape(powerUp.Shape.Position, powerUp.Shape.Extent), img
+        );
+        Assert.AreEqual(playerSpeed.GetType(), expectedPS.GetType());
+    }
+
+    [Test]
+    public void MoreTimeToObjectTest() {
+        var shape = new DynamicShape(new Vec2F(0.1f, 0.1f),
+            new Vec2F(0.2f, 0.2f));
+        var img = new Image(Path.Combine("..", "Assets", "Images", "red-block.png"));
+        var img1 = new Image(Path.Combine("..", "Assets", "Images", "red-block-damaged.png"));
+        PowerUpBlock powerUp = new PowerUpBlock(shape, img, img1, BlockType.PowerUp);
+        Effect moreTime = powerUp.PowerUpTypeToObject(EffectType.MoreTime, img);
+        Effect expectedMT = new MoreTime(
+            new DynamicShape(powerUp.Shape.Position, powerUp.Shape.Extent), img
+        );
+        Assert.AreEqual(moreTime.GetType(), expectedMT.GetType());
+    }
+
+    [Test]
+    public void WideToObjectTest() {
+        var shape = new DynamicShape(new Vec2F(0.1f, 0.1f),
+            new Vec2F(0.2f, 0.2f));
+        var img = new Image(Path.Combine("..", "Assets", "Images", "red-block.png"));
+        var img1 = new Image(Path.Combine("..", "Assets", "Images", "red-block-damaged.png"));
+        PowerUpBlock powerUp = new PowerUpBlock(shape, img, img1, BlockType.PowerUp);
+        Effect wide = powerUp.PowerUpTypeToObject(EffectType.Wide, img);
+        Effect expectedW = new Wide(
+            new DynamicShape(powerUp.Shape.Position, powerUp.Shape.Extent), img
+        );
+        Assert.AreEqual(wide.GetType(), expectedW.GetType());
+    }
+
+    [Test]
+    public void hazardBlockSpawn() {
+        var shape = new DynamicShape(new Vec2F(0.1f, 0.1f),
+            new Vec2F(0.2f, 0.2f));
+        var img = new Image(Path.Combine("..", "Assets", "Images", "red-block.png"));
+        var img1 = new Image(Path.Combine("..", "Assets", "Images", "red-block-damaged.png"));
+        HazardBlock hazard = new HazardBlock(shape, img, img1, BlockType.PowerUp);
+        hazard.SpawnPowerUp();
+        Assert.AreEqual(GameRunning.Effects.CountEntities(), 1);
+    }
+
+    [Test]
+    public void LoseLifeToObjectTest() {
+        var shape = new DynamicShape(new Vec2F(0.1f, 0.1f),
+            new Vec2F(0.2f, 0.2f));
+        var img = new Image(Path.Combine("..", "Assets", "Images", "red-block.png"));
+        var img1 = new Image(Path.Combine("..", "Assets", "Images", "red-block-damaged.png"));
+        HazardBlock hazard = new HazardBlock(shape, img, img1, BlockType.PowerUp);
+        Effect loseLife = hazard.HazardTypeToObject(EffectType.LoseLife, img);
+        Effect expectedLL = new LoseLife(
+            new DynamicShape(hazard.Shape.Position, hazard.Shape.Extent), img
+        );
+        Assert.AreEqual(loseLife.GetType(), expectedLL.GetType());
+    }
+
+    [Test]
+    public void SlownessToObjectTest() {
+        var shape = new DynamicShape(new Vec2F(0.1f, 0.1f),
+            new Vec2F(0.2f, 0.2f));
+        var img = new Image(Path.Combine("..", "Assets", "Images", "red-block.png"));
+        var img1 = new Image(Path.Combine("..", "Assets", "Images", "red-block-damaged.png"));
+        HazardBlock hazard = new HazardBlock(shape, img, img1, BlockType.PowerUp);
+        Effect slow = hazard.HazardTypeToObject(EffectType.Slowness, img);
+        Effect expectedS = new Slowness(
+            new DynamicShape(hazard.Shape.Position, hazard.Shape.Extent), img
+        );
+        Assert.AreEqual(slow.GetType(), expectedS.GetType());
     }
 
 }
